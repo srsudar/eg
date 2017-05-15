334cccc4959d6dc4a768e5919daa1fb22f34d08f
Sam Sudar <sudar.sam@gmail.com>
2017-05-14 19:30:36 -0700

    Rev version to 1.0.1
    Preparing for release, revving version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 # Also bump at eg.eg_util.VERSION
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 LONG_DESCRIPTION = """
 eg provides examples at the command line.
