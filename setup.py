1b749c3ff645e36ab0126f8fbe6cc71d79ecba7f
Sam Sudar <sudar.sam@gmail.com>
2018-04-05 08:53:51 -0700

    Rev version to 1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 # Also bump at eg.eg_util.VERSION
-VERSION = '1.0.2'
+VERSION = '1.1.0'
 
 LONG_DESCRIPTION = """
 eg provides examples at the command line.
