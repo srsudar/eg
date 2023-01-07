f359e7d0e5c2fc384d1de383425c156f3f662bc7
Sam Sudar <sudar.sam@gmail.com>
2023-01-07 17:34:57 -0600

    bump to version 1.2.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 # Also bump at eg.eg_util.VERSION
-VERSION = '1.2.1'
+VERSION = '1.2.2'
 
 LONG_DESCRIPTION = """
 eg provides examples at the command line.
